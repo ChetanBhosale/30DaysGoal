0083b405f6285a1727467c5c3f0556d691ee3702
Chetan <Chetanbhosale810@gmail.com>
2024-06-15 01:59:29 +0530

    remove children from dashboard/page.tsx

--- my-app/src/app/dashboard/layout.tsx
+++ my-app/src/app/dashboard/layout.tsx
@@ -1,29 +1,40 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect } from "react";
 import { useSelector } from "react-redux";
-import Dashboard from "./page";
 import Sidebar from "@/components/dashboard/Sidebar";
 
-type Props = {
+interface Props {
   children: ReactNode;
-};
+}
+
+interface RootState {
+  auth: {
+    user: any; // Replace `any` with the actual type of `user` if known
+  };
+}
 
 const Layout = ({ children }: Props) => {
-  const { user } = useSelector((state: any) => state.auth);
-
+  const { user } = useSelector((state: RootState) => state.auth);
   const router = useRouter();
 
-  if (user) {
-    return (
-      <div className="w-full flex-col 800px:flex-row my-4 flex justify-center gap-4 ">
-        <Sidebar />
-        {children}
-      </div>
-    );
+  useEffect(() => {
+    if (!user) {
+      router.push("/");
+    }
+  }, [user, router]);
+
+  if (!user) {
+    return null; // Optionally, you can return a loading spinner or a message here
   }
-  return router.push("/");
+
+  return (
+    <div className="w-full flex-col 800px:flex-row my-4 flex justify-center gap-4 ">
+      <Sidebar />
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
--- my-app/src/app/dashboard/page.tsx
+++ my-app/src/app/dashboard/page.tsx
@@ -1,17 +1,9 @@
-import Sidebar from "@/components/dashboard/Sidebar";
-import View from "@/components/dashboard/View";
-import { Button } from "@/components/ui/button";
 import React, { FC, ReactNode } from "react";
 
-interface Props {
-  children: ReactNode;
-}
-
-const Dashboard: FC<Props> = ({ children }) => {
+const Dashboard = () => {
   return (
     <div className="w-[50vw] hidden bg-secondary 1000px:flex justify-center items-center">
       <h4 className="text-2xl font-bold">Please select the process</h4>
-      {children && <div className="mt-4">{children}</div>}
     </div>
   );
 };
